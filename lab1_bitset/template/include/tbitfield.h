c84e2c7b6c03812f82ba5aa8af6e8de88ccac319
e-chan-belov <echanbelov@icloud.com>
2024-09-23 16:47:21 +0300

    TBitField and TSet release version 1.0

--- lab1_bitset/template/include/tbitfield.h
+++ lab1_bitset/template/include/tbitfield.h
@@ -25,6 +25,7 @@ private:
   int   GetMemIndex(const int n) const; // индекс в pМем для бита n       (#О2)
   TELEM GetMemMask (const int n) const; // битовая маска для бита n       (#О3)
 public:
+    friend class TSet;
   TBitField(int len);                //                                   (#О1)
   TBitField(const TBitField &bf);    //                                   (#П1)
   ~TBitField();                      //                                    (#С)
--- lab1_bitset/template/include/tset.h
+++ lab1_bitset/template/include/tset.h
@@ -29,13 +29,13 @@ public:
   int operator== (const TSet &s) const; // сравнение
   int operator!= (const TSet &s) const; // сравнение
   TSet& operator=(const TSet &s);  // присваивание
-  TSet operator+ (const int Elem); // объединение с элементом
+  TSet operator+ (const int Elem) const; // объединение с элементом
                                    // элемент должен быть из того же универса
-  TSet operator- (const int Elem); // разность с элементом
+  TSet operator- (const int Elem) const; // разность с элементом
                                    // элемент должен быть из того же универса
-  TSet operator+ (const TSet &s);  // объединение
-  TSet operator* (const TSet &s);  // пересечение
-  TSet operator~ (void);           // дополнение
+  TSet operator+ (const TSet &s) const;  // объединение
+  TSet operator* (const TSet &s) const;  // пересечение
+  TSet operator~ (void) const;           // дополнение
 
   friend istream &operator>>(istream &istr, TSet &bf);
   friend ostream &operator<<(ostream &ostr, const TSet &bf);
