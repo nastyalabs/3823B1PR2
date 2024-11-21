3850dd7cb7180357db9f2715a026aa88898b4dfb
Sanchert <113541aert901@gmail.com>
2024-11-21 20:08:59 +0300

    changed the margins

--- lab1_bitset/KolotukhinAD/template/src/tbitfield.cpp
+++ lab1_bitset/KolotukhinAD/template/src/tbitfield.cpp
@@ -54,64 +54,64 @@ int TBitField::GetMemIndex(const int pos) const noexcept {
 
 // битовая маска для бита n
 int TBitField::GetMemMask (const int pos) const {
-    return 1 << (pos & (bitsInElem - 1));
+	return 1 << (pos & (bitsInElem - 1));
 }
 
 //__________ДОСТУП К БИТАМ БИТОВОГО ПОЛЯ__________
 int TBitField::GetLength() const {
-    return BitLen;
+	return BitLen;
 }
 
 void TBitField::SetBit(const int pos) {
-    if (pos < 0 || pos > BitLen) {
-	    throw 1;
+	if (pos < 0 || pos > BitLen) {
+		throw 1;
 	}
-    int bitInd = pos & (bitsInElem - 1);
-    switch (bitInd) {
-        case 0: pMem[GetMemIndex(pos)].bit0 = 1; break;
-        case 1: pMem[GetMemIndex(pos)].bit1 = 1; break;
-        case 2: pMem[GetMemIndex(pos)].bit2 = 1; break;
-        case 3: pMem[GetMemIndex(pos)].bit3 = 1; break;
-        case 4: pMem[GetMemIndex(pos)].bit4 = 1; break;
-        case 5: pMem[GetMemIndex(pos)].bit5 = 1; break;
-        case 6: pMem[GetMemIndex(pos)].bit6 = 1; break;
-        case 7: pMem[GetMemIndex(pos)].bit7 = 1; break;
-    }
+	int bitInd = pos & (bitsInElem - 1);
+	switch (bitInd) {
+		case 0: pMem[GetMemIndex(pos)].bit0 = 1; break;
+		case 1: pMem[GetMemIndex(pos)].bit1 = 1; break;
+		case 2: pMem[GetMemIndex(pos)].bit2 = 1; break;
+		case 3: pMem[GetMemIndex(pos)].bit3 = 1; break;
+		case 4: pMem[GetMemIndex(pos)].bit4 = 1; break;
+		case 5: pMem[GetMemIndex(pos)].bit5 = 1; break;
+		case 6: pMem[GetMemIndex(pos)].bit6 = 1; break;
+		case 7: pMem[GetMemIndex(pos)].bit7 = 1; break;
+	}
 }
 
 void TBitField::ClrBit(const int pos) {
     if (pos < 0 || pos > BitLen) {
-		  throw 1;
-	    }
-    int bitInd = pos & (bitsInElem - 1);
-    switch (bitInd) {
-        case 0: pMem[GetMemIndex(pos)].bit0 = 0; break;
-        case 1: pMem[GetMemIndex(pos)].bit1 = 0; break;
-        case 2: pMem[GetMemIndex(pos)].bit2 = 0; break;
-        case 3: pMem[GetMemIndex(pos)].bit3 = 0; break;
-        case 4: pMem[GetMemIndex(pos)].bit4 = 0; break;
-        case 5: pMem[GetMemIndex(pos)].bit5 = 0; break;
-        case 6: pMem[GetMemIndex(pos)].bit6 = 0; break;
-        case 7: pMem[GetMemIndex(pos)].bit7 = 0; break;
-    }
+		throw 1;
+	}
+	int bitInd = pos & (bitsInElem - 1);
+	switch (bitInd) {
+		case 0: pMem[GetMemIndex(pos)].bit0 = 0; break;
+		case 1: pMem[GetMemIndex(pos)].bit1 = 0; break;
+		case 2: pMem[GetMemIndex(pos)].bit2 = 0; break;
+		case 3: pMem[GetMemIndex(pos)].bit3 = 0; break;
+		case 4: pMem[GetMemIndex(pos)].bit4 = 0; break;
+		case 5: pMem[GetMemIndex(pos)].bit5 = 0; break;
+		case 6: pMem[GetMemIndex(pos)].bit6 = 0; break;
+		case 7: pMem[GetMemIndex(pos)].bit7 = 0; break;
+	}
 }
 
 int TBitField::GetBit(const int pos) const {
-    if (pos < 0 || pos > BitLen) {
+	if (pos < 0 || pos > BitLen) {
 		throw 1;
 	}
-    int bitInd = pos & (bitsInElem - 1);
-    switch (bitInd) {
-        case 0: return (int)pMem[GetMemIndex(pos)].bit0; break;
-        case 1: return (int)pMem[GetMemIndex(pos)].bit1; break;
-        case 2: return (int)pMem[GetMemIndex(pos)].bit2; break;
-        case 3: return (int)pMem[GetMemIndex(pos)].bit3; break;
-        case 4: return (int)pMem[GetMemIndex(pos)].bit4; break;
-        case 5: return (int)pMem[GetMemIndex(pos)].bit5; break;
-        case 6: return (int)pMem[GetMemIndex(pos)].bit6; break;
-        case 7: return (int)pMem[GetMemIndex(pos)].bit7; break;
-    }
-    return -1;
+	int bitInd = pos & (bitsInElem - 1);
+	switch (bitInd) {
+		case 0: return (int)pMem[GetMemIndex(pos)].bit0; break;
+		case 1: return (int)pMem[GetMemIndex(pos)].bit1; break;
+		case 2: return (int)pMem[GetMemIndex(pos)].bit2; break;
+		case 3: return (int)pMem[GetMemIndex(pos)].bit3; break;
+		case 4: return (int)pMem[GetMemIndex(pos)].bit4; break;
+		case 5: return (int)pMem[GetMemIndex(pos)].bit5; break;
+		case 6: return (int)pMem[GetMemIndex(pos)].bit6; break;
+		case 7: return (int)pMem[GetMemIndex(pos)].bit7; break;
+	}
+	return -1;
 }
 
 // __________БИТОВЫЕ ОПЕРАЦИИ__________
@@ -119,15 +119,15 @@ int TBitField::GetBit(const int pos) const {
 // присваивание
 TBitField& TBitField::operator=(const TBitField &_other) {
 	if (this != &_other) {
-        delete[] pMem;
-        MemLen = _other.MemLen;
-        BitLen = _other.BitLen;
-        pMem = new TELEM[MemLen];
+		delete[] pMem;
+		MemLen = _other.MemLen;
+		BitLen = _other.BitLen;
+		pMem = new TELEM[MemLen];
 		for (int i = 0; i < MemLen; i++) {
 			pMem[i] = _other.pMem[i];
 		}
 	}
-    return *this;
+	return *this;
 }
 
 // сравнение
@@ -149,7 +149,6 @@ bool TBitField::operator!=(const TBitField &_other) const {
 		return true;
 	}
 	for (int i = 0; i < BitLen; i++) {
-		cout << "\n\n\n this " << this->GetBit(i) << "  other " << _other.GetBit(i) << "\n\n\n";
 		if (this->GetBit(i) != _other.GetBit(i)) {
 			return true;
 		}
@@ -160,43 +159,43 @@ bool TBitField::operator!=(const TBitField &_other) const {
 // операция "или"
 TBitField TBitField::operator|(const TBitField &_other) {
 	int maxBitLen = max(BitLen, _other.BitLen);
-    TBitField result(maxBitLen);
-    for (int i = 0; i < result.MemLen; i++) {
-        result.pMem[i].bit0 = (i < MemLen ? pMem[i].bit0 : 0) || (i < _other.MemLen ? _other.pMem[i].bit0 : 0);
+	TBitField result(maxBitLen);
+	for (int i = 0; i < result.MemLen; i++) {
+		result.pMem[i].bit0 = (i < MemLen ? pMem[i].bit0 : 0) || (i < _other.MemLen ? _other.pMem[i].bit0 : 0);
 		result.pMem[i].bit1 = (i < MemLen ? pMem[i].bit1 : 0) || (i < _other.MemLen ? _other.pMem[i].bit1 : 0);
 		result.pMem[i].bit2 = (i < MemLen ? pMem[i].bit2 : 0) || (i < _other.MemLen ? _other.pMem[i].bit2 : 0);
-        result.pMem[i].bit3 = (i < MemLen ? pMem[i].bit3 : 0) || (i < _other.MemLen ? _other.pMem[i].bit3 : 0);
- 	    result.pMem[i].bit4 = (i < MemLen ? pMem[i].bit4 : 0) || (i < _other.MemLen ? _other.pMem[i].bit4 : 0);
+		result.pMem[i].bit3 = (i < MemLen ? pMem[i].bit3 : 0) || (i < _other.MemLen ? _other.pMem[i].bit3 : 0);
+		result.pMem[i].bit4 = (i < MemLen ? pMem[i].bit4 : 0) || (i < _other.MemLen ? _other.pMem[i].bit4 : 0);
 		result.pMem[i].bit5 = (i < MemLen ? pMem[i].bit5 : 0) || (i < _other.MemLen ? _other.pMem[i].bit5 : 0);
 		result.pMem[i].bit6 = (i < MemLen ? pMem[i].bit6 : 0) || (i < _other.MemLen ? _other.pMem[i].bit6 : 0);
 		result.pMem[i].bit7 = (i < MemLen ? pMem[i].bit7 : 0) || (i < _other.MemLen ? _other.pMem[i].bit7 : 0);
-    }
-    return result;
+	}
+	return result;
 }
 
 // операция "и"
 TBitField TBitField::operator&(const TBitField &_other) {
 	int maxLen = max(BitLen, _other.BitLen);
-    TBitField result(maxLen);
-    for (int i = 0; i < result.MemLen; i++) {
-        result.pMem[i].bit0 = (i <        MemLen ?        pMem[i].bit0 : 0) &&
-						      (i < _other.MemLen ? _other.pMem[i].bit0 : 0);
+	TBitField result(maxLen);
+	for (int i = 0; i < result.MemLen; i++) {
+		result.pMem[i].bit0 = (i <        MemLen ?        pMem[i].bit0 : 0) &&
+							  (i < _other.MemLen ? _other.pMem[i].bit0 : 0);
 		result.pMem[i].bit1 = (i <        MemLen ?        pMem[i].bit1 : 0) &&
-						      (i < _other.MemLen ? _other.pMem[i].bit1 : 0);
+							  (i < _other.MemLen ? _other.pMem[i].bit1 : 0);
 		result.pMem[i].bit2 = (i <        MemLen ?        pMem[i].bit2 : 0) &&
-						      (i < _other.MemLen ? _other.pMem[i].bit2 : 0);
-        result.pMem[i].bit3 = (i <        MemLen ?        pMem[i].bit3 : 0) &&
-	     				      (i < _other.MemLen ? _other.pMem[i].bit3 : 0);
- 	    result.pMem[i].bit4 = (i <        MemLen ?        pMem[i].bit4 : 0) &&
-						      (i < _other.MemLen ? _other.pMem[i].bit4 : 0);
+							  (i < _other.MemLen ? _other.pMem[i].bit2 : 0);
+		result.pMem[i].bit3 = (i <        MemLen ?        pMem[i].bit3 : 0) &&
+							  (i < _other.MemLen ? _other.pMem[i].bit3 : 0);
+		result.pMem[i].bit4 = (i <        MemLen ?        pMem[i].bit4 : 0) &&
+							  (i < _other.MemLen ? _other.pMem[i].bit4 : 0);
 		result.pMem[i].bit5 = (i <        MemLen ?        pMem[i].bit5 : 0) &&
-						      (i < _other.MemLen ? _other.pMem[i].bit5 : 0);
+							  (i < _other.MemLen ? _other.pMem[i].bit5 : 0);
 		result.pMem[i].bit6 = (i <        MemLen ?        pMem[i].bit6 : 0) &&
-						      (i < _other.MemLen ? _other.pMem[i].bit6 : 0);
+							  (i < _other.MemLen ? _other.pMem[i].bit6 : 0);
 		result.pMem[i].bit7 = (i <        MemLen ?        pMem[i].bit7 : 0) &&
-						      (i < _other.MemLen ? _other.pMem[i].bit7 : 0);
-    }
-    return result;
+							  (i < _other.MemLen ? _other.pMem[i].bit7 : 0);
+	}
+	return result;
 }
 
 // отрицание
@@ -207,19 +206,19 @@ TBitField TBitField::operator~(void) {
 			result.SetBit(i);
 		}
 	}
-    return result;
+	return result;
 }
 
 // ввод
 istream& operator>>(istream &is, TBitField &_this) {
 	for (int i = 0; i < _this.BitLen; i++) {
-        int bit;
-        is >> bit;
-        if (bit) {
+		int bit;
+		is >> bit;
+		if (bit) {
 			_this.SetBit(i);
 		}
-    }
-    return is;
+	}
+	return is;
 }
 
 // вывод
